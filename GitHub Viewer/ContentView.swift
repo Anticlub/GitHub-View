e5df991cacbc257d7b3626fdfeb5f488083e84a6
Anticlub <cristofdzfdz@gmail.com>
2026-02-11 14:32:12 +0100

    Initial Commit

---
+++ GitHub Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Viewer
+//
+//  Created by cristofer fernandez on 11/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Viewer/GitHub_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ViewerApp.swift
+//  GitHub Viewer
+//
+//  Created by cristofer fernandez on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
