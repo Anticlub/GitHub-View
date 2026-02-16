9690cc2de8eb55fbd837a8011eb7f8ba7879406f
Anticlub <cristofdzfdz@gmail.com>
2026-02-16 11:59:23 +0100

    Primer commit con media app hecha, tengo q hacerlos cada poco, la app ahora mismo tiene una homeView con un TextField y un Button para buscar, hago la consulta a la api de github y guardo los datos en un array, luego lo muestro en RepoDetailview

---
+++ GitHub Viewer/Features/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Viewer
+//
+//  Created by cristofer fernandez on 11/2/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    HomeView()
+}
